7b99a9f2b80657b526f7300e7b41c18b31c78a39
Ali Habibian <ali.habibian04@gmail.com>
2024-12-02 02:52:59 +0330

    create version 1.1.0

--- assets/js/swalalert.js
+++ assets/js/swalalert.js
@@ -1,12 +1,16 @@
+// assets/js/swalalert.js
+
 import Swal from 'sweetalert2';
 
 document.addEventListener('DOMContentLoaded', function () {
-@if(session('swalalert'))
-        const alertData = @json(session('swalalert'));
-    Swal.fire({
-        title: alertData.message,
-        icon: alertData.type,
-        confirmButtonText: "{{ config('swalalert.confirm_button_text', 'OK') }}",
-    });
-@endif
+    // Use a conditional check for the session data and assign it to the variable
+    const alertData = @json(session('swalalert', null));
+
+    if (alertData) {
+        Swal.fire({
+            title: alertData.message,
+            icon: alertData.type,
+            confirmButtonText: "{{ config('swalalert.confirm_button_text', 'OK') }}",
+        });
+    }
 });
